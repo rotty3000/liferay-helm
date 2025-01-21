8f0ebb6427de2ef3e3badf0d5d681a413bfbaedf
Raymond Augé <raymond.auge@liferay.com>
2025-01-21 09:21:25 -0500

    make sure file ref has proper base

--- .github/scripts/publish_helm_index.sh
+++ .github/scripts/publish_helm_index.sh
@@ -38,7 +38,7 @@ gh api graphql \
 	-F branchName=${PUBLISH_BRANCH} \
 	-F expectedHeadOid=$(git rev-parse HEAD) \
 	-F commitMessage="github-actions[bot] commit updated helm index" \
-	-F 'query=@.github/api/createCommitOnBranch.gql' \
+	-F "query=@${SOURCE_DIR}/.github/api/createCommitOnBranch.gql" \
 	${FILES}
 
 popd >& /dev/null
